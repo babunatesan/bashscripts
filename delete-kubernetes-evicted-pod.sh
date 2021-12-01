3f2d5f233da4154cc7e6a57828a06c4482c0d71a
babunatesan <95180551+babunatesan@users.noreply.github.com>
2021-12-01 19:52:14 +0530

    Update delete-kubernetes-evicted-pod.sh

--- delete-kubernetes-evicted-pod.sh
+++ delete-kubernetes-evicted-pod.sh
@@ -1,2 +1,2 @@
 # we need change the namespace, replace the namespace next -n [digimops] to your namespace.
-kubectl get pod -n digimops | grep Evicted | awk '{print $1}' | xargs kubectl delete pod -n digimops
+kubectl get pod -n {namespace} | grep Evicted | awk '{print $1}' | xargs kubectl delete pod -n {namespace}
