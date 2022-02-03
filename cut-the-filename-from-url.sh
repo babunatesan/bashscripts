616da63d45928285219e9771446059edc871f239
babunatesan <95180551+babunatesan@users.noreply.github.com>
2022-02-03 11:13:32 +0530

    Create cut-the-filename-from-url.sh

---
+++ cut-the-filename-from-url.sh
@@ -0,0 +1,2 @@
+#URL: "https://gitlab.<domainname>.net/xyzlmn/foo-too-usa-mmv-remove-expectation"
+echo "URL HERE" | cut -d/ -f5 #delemeter is / and cutting the 5th element from the git url, based on your url you have to change -F[N] value in cut command
