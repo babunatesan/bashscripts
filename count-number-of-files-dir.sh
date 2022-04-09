7c84087e553601941ac9b13303531c189775a431
babunatesan <95180551+babunatesan@users.noreply.github.com>
2022-04-09 17:59:23 +0530

    count method changed

--- count-number-of-files-dir.sh
+++ count-number-of-files-dir.sh
@@ -4,6 +4,8 @@
 input="dir_name.txt"
 while IFS= read -r line
 do
-  find $line -type f | wc -l > $line.txt
-  echo `cat @line.txt`
+  #find $line -type f | wc -l > $line.txt
+  #echo `cat @line.txt`
+  echo "$line"
+  ls -l $line/digimop-operations/workflow-instances | grep -c ^d
 done < "$input"
