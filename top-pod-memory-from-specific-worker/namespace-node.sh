c66dfe0e89b6c4147dea9150c5ed3249c6907043
Babu Natesan <95180551+babunatesan@users.noreply.github.com>
2024-02-20 18:57:24 +0530

    Delete top-pod-memory-from-specific-worker/namespace-node.sh

--- top-pod-memory-from-specific-worker/namespace-node.sh
+++
@@ -1,5 +0,0 @@
-echo "Enter the workernode name"
-read worker
-echo "Enter the namespace"
-read namespace
-kubectl get po -o wide -n $namespace | grep $worker | awk '{print $1}' | xargs -I{} kubectl top po {} -n $namespace --sort-by=memory
